6c4c9c6602dfb52c9c3252a8ecab0602f99e4f8a
Matthew Jendong <mjendong@ufl.edu>
2026-04-08 10:32:23 -0400

    git add.calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,27 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
 
-One function per operation, in order.
-"""
-# First example
-def add(a, b): 
-    pass
+import math
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+   return a * b
+
+def divide(a, b):
+    if a == 0:
+        return ZeroDivisionError
+    return b / a
+
+def logarithm(a, b):
+    return log(a,b)
+
+    # use math library/raise ValueError
+
+def exponent(a, b):
+    return a**b
+
 
 
