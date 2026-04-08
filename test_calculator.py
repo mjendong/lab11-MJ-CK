18a9bcc104eed3f648534f671d3df348b3c3f706
Matthew Jendong <mjendong@ufl.edu>
2026-04-08 10:59:52 -0400

    git add test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -11,12 +11,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self):
+        # 3 assertions for multiplication
+        self.assertEqual(self.calc.multiply(3, 7), 21)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 100), 0)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_divide(self):
+        # 3 assertions for division
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-6, 3), -2)
+        # Testing float division
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
+    # ############################
+
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -32,24 +40,29 @@ class TestCalculator(unittest.TestCase):
     #     # use same technique from test_divide_by_zero
     #     fill in code
     # ##########################
-    
+
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        # 1 assertion: log of 0 or negative numbers is undefined
+        with self.assertRaises(ValueError):
+            self.calc.logarithm(0, 5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        # 3 assertions: common Pythagorean triples
+        self.assertEqual(self.calc.hypotenuse(3, 4), 5)
+        self.assertEqual(self.calc.hypotenuse(5, 12), 13)
+        self.assertEqual(self.calc.hypotenuse(8, 15), 17)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self):
+        # 1. Test for invalid argument (negative number)
+        with self.assertRaises(ValueError):
+            self.calc.square_root(-4)
+
+        # 2 & 3. Test basic function
+        self.assertEqual(self.calc.square_root(9), 3)
+        self.assertEqual(self.calc.square_root(100), 10)
+    ##############################
+
 
 # Do not touch this
 if __name__ == "__main__":
